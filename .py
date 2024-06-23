b2df91def20732ef00042e7d22e913d98b55cd24
nileshdeb <debnilesh69@gmail.com>
2024-06-23 21:04:51 +0530

    first

--- .py
+++ .py
@@ -1,1 +1,6 @@
-#fugilgi;h
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+if age >= 18:
+ print("Welcome,", name, "! You are eligible.")
+else:
+ print("Sorry,", name, ". Please come back when you are older.")
